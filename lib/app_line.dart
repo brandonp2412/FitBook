d5c3b62239f6da24ce2ad0e5712fbe3a39e6f88e
Brandon Presley <brandon@presley.nz>
2024-06-25 15:08:45 +1200

    Limit # of body weight entries

--- lib/app_line.dart
+++ lib/app_line.dart
@@ -3,8 +3,8 @@ import 'package:fit_book/constants.dart';
 import 'package:fit_book/main.dart';
 import 'package:fit_book/settings/settings_state.dart';
 import 'package:fl_chart/fl_chart.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/material.dart' as material;
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
@@ -37,7 +37,8 @@ class AppLine extends StatefulWidget {
 class _AppLineState extends State<AppLine> {
   late Stream<List<GraphData>> _graphStream;
   late SettingsState _settings;
-  final formatter = NumberFormat('#,##0.00');
+  final _formatter = NumberFormat('#,##0.00');
+  final _limit = 11;
 
   @override
   void didUpdateWidget(covariant AppLine oldWidget) {
@@ -98,7 +99,8 @@ class _AppLineState extends State<AppLine> {
               db.weights.amount,
               db.weights.unit,
             ])
-            ..groupBy([createdCol]))
+            ..groupBy([createdCol])
+            ..limit(_limit))
           .watch()
           .map(
             (results) => results
@@ -127,7 +129,7 @@ class _AppLineState extends State<AppLine> {
               ),
             ])
             ..groupBy([createdCol])
-            ..limit(11))
+            ..limit(_limit))
           .watch()
           .map((results) {
         return results.map((result) {
@@ -291,7 +293,7 @@ class _AppLineState extends State<AppLine> {
                   child: ListTile(
                     title: const Text("Average"),
                     subtitle: Text(
-                      "${formatter.format(average)} ${rows.first.unit}",
+                      "${_formatter.format(average)} ${rows.first.unit}",
                     ),
                     leading: Radio(
                       value: 1,
@@ -307,7 +309,7 @@ class _AppLineState extends State<AppLine> {
                   child: ListTile(
                     title: const Text("Goal"),
                     subtitle: Text(
-                      "${formatter.format(goal)} ${rows.first.unit}",
+                      "${_formatter.format(goal)} ${rows.first.unit}",
                     ),
                     leading: Radio(
                       value: 1,
@@ -373,7 +375,7 @@ class _AppLineState extends State<AppLine> {
         final row = rows.elementAt(touchedSpots.first.spotIndex);
         final created =
             DateFormat(_settings.shortDateFormat).format(row.created);
-        final value = formatter.format(row.value);
+        final value = _formatter.format(row.value);
 
         String text = "$value $unit\n";
 
