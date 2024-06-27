4e7c23544252406f898efafb3bec6cc7cc8682f1
Brandon Presley <brandon@presley.nz>
2024-06-27 13:01:31 +1200

    Last background commit

--- lib/background.dart
+++ lib/background.dart
@@ -4,7 +4,7 @@ import 'package:fit_book/main.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void registerBackground() async {
-  await BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
+  await BackgroundFetch.registerHeadlessTask(notify);
   await BackgroundFetch.configure(
       BackgroundFetchConfig(
         minimumFetchInterval: 15,
@@ -17,7 +17,7 @@ void registerBackground() async {
         requiredNetworkType: NetworkType.NONE,
       ), (String taskId) async {
     print("[BackgroundFetch] Event received $taskId");
-    backgroundFetchHeadlessTask(HeadlessTask(taskId, false));
+    await notify(HeadlessTask(taskId, false));
     BackgroundFetch.finish(taskId);
   }, (String taskId) async {
     print("[BackgroundFetch] TASK TIMEOUT taskId: $taskId");
@@ -32,7 +32,7 @@ void registerBackground() async {
 }
 
 @pragma('vm:entry-point')
-void backgroundFetchHeadlessTask(HeadlessTask task) async {
+Future<void> notify(HeadlessTask task) async {
   String taskId = task.taskId;
   bool isTimeout = task.timeout;
   if (isTimeout) {
@@ -43,76 +43,111 @@ void backgroundFetchHeadlessTask(HeadlessTask task) async {
   print('[BackgroundFetch] Headless event received.');
 
   const settings = AndroidInitializationSettings(
-    '@drawable/nutrition',
+    '@mipmap/ic_launcher',
   );
   const initialization = InitializationSettings(android: settings);
-  final notifyPlugin = FlutterLocalNotificationsPlugin();
-  await notifyPlugin.initialize(initialization);
+  final plugin = FlutterLocalNotificationsPlugin();
+  await plugin.initialize(initialization);
+  print('[BackgroundFetch] Initialized notify plugin.');
 
-  final now = DateTime.now();
-  if (now.hour >= 7 && now.hour < 9) {
-    final startOfBreakfast = DateTime(now.year, now.month, now.day, 7);
-    final endOfBreakfast = DateTime(now.year, now.month, now.day, 9);
-    final entries = await (db.entries.select()
-          ..where(
-            (u) => u.created.isBetweenValues(startOfBreakfast, endOfBreakfast),
-          ))
-        .get();
-    if (entries.isNotEmpty) return;
+  const details = NotificationDetails(
+    android: AndroidNotificationDetails(
+      'background_debug',
+      'Debug channel',
+    ),
+  );
+  await plugin.show(
+    3,
+    'Running background',
+    "We are running in the background...",
+    details,
+  );
 
-    const details = NotificationDetails(
-      android: AndroidNotificationDetails(
-        'breakfast_reminders',
-        'Breakfast reminders',
-      ),
-    );
-    await notifyPlugin.show(
-      0,
-      'Breakfast',
-      "Don't forget to log your breakfast.",
-      details,
-    );
-  } else if (now.hour >= 12 && now.hour < 14) {
-    final startOfLunch = DateTime(now.year, now.month, now.day, 12);
-    final endOfLunch = DateTime(now.year, now.month, now.day, 14);
-    final entries = await (db.entries.select()
-          ..where((u) => u.created.isBetweenValues(startOfLunch, endOfLunch)))
-        .get();
-    if (entries.isNotEmpty) return;
+  await breakfast(plugin);
+  await _lunch(plugin);
+  await _dinner(plugin);
 
-    const details = NotificationDetails(
-      android: AndroidNotificationDetails(
-        'lunch_reminders',
-        'Lunch reminders',
-      ),
-    );
-    await notifyPlugin.show(
-      1,
-      'Lunch',
-      "Don't forget to log your lunch.",
-      details,
-    );
-  } else if (now.hour >= 19 && now.hour < 21) {
-    final startOfDinner = DateTime(now.year, now.month, now.day, 19);
-    final endOfDinner = DateTime(now.year, now.month, now.day, 21);
-    final entries = await (db.entries.select()
-          ..where((u) => u.created.isBetweenValues(startOfDinner, endOfDinner)))
-        .get();
-    if (entries.isNotEmpty) return;
-
-    const details = NotificationDetails(
-      android: AndroidNotificationDetails(
-        'lunch_reminders',
-        'Lunch reminders',
-      ),
-    );
-    await notifyPlugin.show(
-      2,
-      'Dinner',
-      "Don't forget to log your dinner.",
-      details,
-    );
-  }
+  // final now = DateTime.now();
+  // print('[BackgroundFetch] hour=${now.hour}');
+  // if (now.hour >= 7 && now.hour < 9)
+  //   await breakfast(plugin);
+  // else if (now.hour >= 12 && now.hour < 14)
+  //   await _lunch(plugin);
+  // else if (now.hour >= 19 && now.hour < 21) await _dinner(plugin);
 
   BackgroundFetch.finish(taskId);
+}
+
+Future<void> breakfast(FlutterLocalNotificationsPlugin plugin) async {
+  final now = DateTime.now();
+  final startOfBreakfast = DateTime(now.year, now.month, now.day, 7);
+  final endOfBreakfast = DateTime(now.year, now.month, now.day, 9);
+  final entries = await (db.entries.select()
+        ..where(
+          (u) => u.created.isBetweenValues(startOfBreakfast, endOfBreakfast),
+        ))
+      .get();
+  print('[BackgroundFetch] breakfast.length=${entries.length}');
+  if (entries.isNotEmpty) return;
+
+  const details = NotificationDetails(
+    android: AndroidNotificationDetails(
+      'breakfast_reminders',
+      'Breakfast reminders',
+    ),
+  );
+  await plugin.show(
+    0,
+    'Breakfast',
+    "Don't forget to log your breakfast.",
+    details,
+  );
+}
+
+Future<void> _lunch(FlutterLocalNotificationsPlugin plugin) async {
+  final now = DateTime.now();
+  final startOfLunch = DateTime(now.year, now.month, now.day, 12);
+  final endOfLunch = DateTime(now.year, now.month, now.day, 14);
+  final entries = await (db.entries.select()
+        ..where((u) => u.created.isBetweenValues(startOfLunch, endOfLunch)))
+      .get();
+  print('[BackgroundFetch] lunch.length=${entries.length}');
+  if (entries.isNotEmpty) return;
+
+  const details = NotificationDetails(
+    android: AndroidNotificationDetails(
+      'lunch_reminders',
+      'Lunch reminders',
+    ),
+  );
+  await plugin.show(
+    1,
+    'Lunch',
+    "Don't forget to log your lunch.",
+    details,
+  );
+}
+
+Future<void> _dinner(FlutterLocalNotificationsPlugin plugin) async {
+  final now = DateTime.now();
+  final startOfDinner = DateTime(now.year, now.month, now.day, 19);
+  final endOfDinner = DateTime(now.year, now.month, now.day, 21);
+  final entries = await (db.entries.select()
+        ..where((u) => u.created.isBetweenValues(startOfDinner, endOfDinner)))
+      .get();
+  print('[BackgroundFetch] dinner.length=${entries.length}');
+  if (entries.isNotEmpty) return;
+
+  const details = NotificationDetails(
+    android: AndroidNotificationDetails(
+      'lunch_reminders',
+      'Lunch reminders',
+    ),
+  );
+  await plugin.show(
+    2,
+    'Dinner',
+    "Don't forget to log your dinner.",
+    details,
+  );
 }
