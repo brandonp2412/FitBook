edef6f2f049eea0df9c9c995bf309c8f9f42d188
Brandon Presley <brandon@presley.nz>
2024-07-12 18:04:12 +1200

    Use commits as default release notes

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,21 @@
 
 set -ex
 
-IFS='+.' read -r major minor patch build_number <<< "$(yq -r .version pubspec.yaml)"
+IFS='+.' read -r major minor patch build_number <<<"$(yq -r .version pubspec.yaml)"
 new_patch=$((patch + 1))
 new_build_number=$((build_number + 1))
 changelog_number=$((new_build_number * 10 + 3))
 new_flutter_version="$major.$minor.$new_patch+$new_build_number"
 new_version="$major.$minor.$new_patch"
 
-nvim "fastlane/metadata/android/en-US/changelogs/$changelog_number.txt"
-changelog=$(cat "fastlane/metadata/android/en-US/changelogs/$changelog_number.txt")
-echo "$changelog" > $HOME/fitbook/fastlane/metadata/android/en-US/changelogs/$changelog_number.txt 
-echo "$changelog" > fastlane/metadata/en-AU/release_notes.txt
-echo "$changelog" > fastlane/metadata/en-US/release_notes.txt
+changelog_file="fastlane/metadata/android/en-US/changelogs/$changelog_number.txt"
+git --no-pager log --pretty=format:'%s' "$(git describe --tags --abbrev=0)"..HEAD |
+  awk '{print "- "$0}' >$changelog_file
+nvim "$changelog_file"
+changelog=$(cat "$changelog_file")
+echo "$changelog" >"$changelog_file"
+echo "$changelog" >fastlane/metadata/en-AU/release_notes.txt
+echo "$changelog" >fastlane/metadata/en-US/release_notes.txt
 
 flutter test
 dart analyze lib
@@ -26,7 +29,6 @@ dart format --set-exit-if-changed lib
 yq -yi ".version |= \"$new_flutter_version\"" pubspec.yaml
 git add pubspec.yaml
 git add fastlane/metadata
-last_commit=$(git log -1 --pretty=%B | head -n 1)
 git commit -m "$new_version 🚀
 
 $changelog"
@@ -43,7 +45,7 @@ mv $apk/app-release.apk $apk/fitbook.apk
 
 git push
 
-gh release create "$new_version" --notes "$changelog"  \
+gh release create "$new_version" --notes "$changelog" \
   $apk/app-*-release.apk \
   $apk/pipeline/linux/x64/release/bundle/fitbook-linux.zip \
   $apk/fitbook.apk
