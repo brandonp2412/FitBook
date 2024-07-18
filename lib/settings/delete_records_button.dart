6f78d80807043199b1c86ffe47ea84ab60a55630
Brandon Presley <brandon@presley.nz>
2024-07-18 13:19:20 +1200

    Add delete database button

--- lib/settings/delete_records_button.dart
+++ lib/settings/delete_records_button.dart
@@ -1,4 +1,5 @@
 import 'package:drift/drift.dart';
+import 'package:fit_book/constants.dart';
 import 'package:fit_book/main.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,137 @@ class DeleteRecordsButton extends StatelessWidget {
     super.key,
     required this.pageContext,
   });
+
+  deleteWeights(BuildContext sheetContext) {
+    Navigator.pop(sheetContext);
+    showDialog(
+      context: sheetContext,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: const Text('Confirm delete'),
+          content: const Text(
+            'Are you sure you want to delete all weights? This action is not reversible.',
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(dialogContext);
+              },
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () async {
+                Navigator.pop(dialogContext);
+                await db.weights.deleteAll();
+                if (pageContext.mounted) Navigator.pop(pageContext);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  deleteDatabase(BuildContext sheetContext) {
+    Navigator.pop(sheetContext);
+    showDialog(
+      context: sheetContext,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: const Text('Confirm delete'),
+          content: const Text(
+            'Are you sure you want to delete everything? This action is not reversible.',
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(dialogContext);
+              },
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () async {
+                Navigator.pop(dialogContext);
+                db.transaction(() async {
+                  for (final table in db.allTables)
+                    await db.customStatement(
+                      'DELETE FROM ${table.actualTableName}',
+                    );
+                  await db.settings.insertOne(defaultSettings);
+                });
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  deleteFoods(BuildContext sheetContext) {
+    Navigator.pop(sheetContext);
+    showDialog(
+      context: sheetContext,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: const Text('Confirm delete'),
+          content: const Text(
+            'Are you sure you want to delete all food & entries? This action is not reversible.',
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(dialogContext);
+              },
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () async {
+                Navigator.pop(dialogContext);
+                await db.entries.deleteAll();
+                await db.foods.deleteAll();
+                if (pageContext.mounted) Navigator.pop(pageContext);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  deleteDiary(BuildContext sheetContext) {
+    Navigator.pop(sheetContext);
+    showDialog(
+      context: sheetContext,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: const Text('Confirm delete'),
+          content: const Text(
+            'Are you sure you want to delete all entries? This action is not reversible.',
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(dialogContext);
+              },
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () async {
+                Navigator.pop(dialogContext);
+                await db.entries.deleteAll();
+                if (!pageContext.mounted) return;
+                Navigator.pop(pageContext);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,108 +154,22 @@ class DeleteRecordsButton extends StatelessWidget {
                 ListTile(
                   leading: const Icon(Icons.date_range),
                   title: const Text('Diary'),
-                  onTap: () async {
-                    Navigator.pop(sheetContext);
-                    showDialog(
-                      context: sheetContext,
-                      builder: (BuildContext dialogContext) {
-                        return AlertDialog(
-                          title: const Text('Confirm delete'),
-                          content: const Text(
-                            'Are you sure you want to delete all entries? This action is not reversible.',
-                          ),
-                          actions: <Widget>[
-                            TextButton(
-                              child: const Text('Cancel'),
-                              onPressed: () {
-                                Navigator.pop(dialogContext);
-                              },
-                            ),
-                            TextButton(
-                              child: const Text('Delete'),
-                              onPressed: () async {
-                                Navigator.pop(dialogContext);
-                                await db.entries.deleteAll();
-                                if (!pageContext.mounted) return;
-                                Navigator.pop(pageContext);
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
+                  onTap: () => deleteDiary(sheetContext),
                 ),
                 ListTile(
                   leading: const Icon(Icons.restaurant),
                   title: const Text('Food'),
-                  onTap: () async {
-                    Navigator.pop(sheetContext);
-                    showDialog(
-                      context: sheetContext,
-                      builder: (BuildContext dialogContext) {
-                        return AlertDialog(
-                          title: const Text('Confirm delete'),
-                          content: const Text(
-                            'Are you sure you want to delete all food & entries? This action is not reversible.',
-                          ),
-                          actions: <Widget>[
-                            TextButton(
-                              child: const Text('Cancel'),
-                              onPressed: () {
-                                Navigator.pop(dialogContext);
-                              },
-                            ),
-                            TextButton(
-                              child: const Text('Delete'),
-                              onPressed: () async {
-                                Navigator.pop(dialogContext);
-                                await db.entries.deleteAll();
-                                await db.foods.deleteAll();
-                                if (pageContext.mounted)
-                                  Navigator.pop(pageContext);
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
+                  onTap: () => deleteFoods(sheetContext),
                 ),
                 ListTile(
                   leading: const Icon(Icons.scale),
                   title: const Text('Weight'),
-                  onTap: () async {
-                    Navigator.pop(sheetContext);
-                    showDialog(
-                      context: sheetContext,
-                      builder: (BuildContext dialogContext) {
-                        return AlertDialog(
-                          title: const Text('Confirm delete'),
-                          content: const Text(
-                            'Are you sure you want to delete all weights? This action is not reversible.',
-                          ),
-                          actions: <Widget>[
-                            TextButton(
-                              child: const Text('Cancel'),
-                              onPressed: () {
-                                Navigator.pop(dialogContext);
-                              },
-                            ),
-                            TextButton(
-                              child: const Text('Delete'),
-                              onPressed: () async {
-                                Navigator.pop(dialogContext);
-                                await db.weights.deleteAll();
-                                if (pageContext.mounted)
-                                  Navigator.pop(pageContext);
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
+                  onTap: () => deleteWeights(sheetContext),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.storage),
+                  title: const Text('Database'),
+                  onTap: () => deleteDatabase(sheetContext),
                 ),
               ],
             );
