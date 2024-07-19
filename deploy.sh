bf2e71d6ffbb324e0392f0c69091ed370f7ed9cd
Brandon Presley <brandon@presley.nz>
2024-07-19 13:13:11 +1200

    Build for windows in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,11 @@ new_build_number=$((build_number + 1))
 changelog_number=$((new_build_number * 10 + 3))
 new_flutter_version="$major.$minor.$new_patch+$new_build_number"
 new_version="$major.$minor.$new_patch"
+apk=$PWD/build/app/outputs/flutter-apk
+
+IFS='+.' read -r msix_major msix_minor msix_patch msix_zero <<<"$(yq -r .msix_config.msix_version pubspec.yaml)"
+new_msix_patch=$((msix_patch + 1))
+new_msix_version="$msix_major.$msix_minor.$new_msix_patch+$msix_zero"
 
 changelog_file="fastlane/metadata/android/en-US/changelogs/$changelog_number.txt"
 if ! [ -f $changelog_file ]; then
@@ -36,6 +41,7 @@ dart format --set-exit-if-changed lib
 ./screenshots.sh "tenInchScreenshots"
 
 yq -yi ".version |= \"$new_flutter_version\"" pubspec.yaml
+yq -yi ".msix_config.msix_version |= \"$new_msix_version\"" pubspec.yaml
 git add pubspec.yaml
 git add fastlane/metadata
 git commit -m "$new_version 🚀
@@ -44,20 +50,31 @@ $changelog"
 
 flutter build apk --split-per-abi
 flutter build apk
+mv "$apk"/app-release.apk "$apk"/fitbook.apk
 flutter build appbundle
+
 mkdir -p build/native_assets/linux
 flutter build linux
-
-apk=$PWD/build/app/outputs/flutter-apk
-(cd $apk/pipeline/linux/x64/release/bundle && zip -r fitbook-linux.zip .)
-mv $apk/app-release.apk $apk/fitbook.apk
+(cd "$apk/pipeline/linux/x64/release/bundle" && zip -r fitbook-linux.zip .)
+docker start windows
+sshpass -p gates ssh windows "cd FitBook && \
+git pull && \
+mkdir C:\\Users\\brandon\\FitBook\\assets || echo skipping && \
+copy assets\\ic_launcher.png C:\\Users\\brandon\\FitBook\\assets && \
+dart run msix:create && \
+rmdir /S /Q \\\\host.lan\\Data\\FitBook || echo skipping && \
+mkdir \\\\host.lan\\Data\\FitBook && \
+move build\\windows\\x64\\runner\\Release\\fit_book.msix \\\\host.lan\\Data && \
+xcopy build\\windows\\x64\\runner\\Release \\\\host.lan\\Data\\FitBook /s /e /h"
+sudo chown -R "$USER" "$HOME"/windows/FitBook
+(cd "$HOME"/windows/FitBook && zip -r fitbook-windows.zip .)
 
 git push
-
 gh release create "$new_version" --notes "$changelog" \
-  $apk/app-*-release.apk \
-  $apk/pipeline/linux/x64/release/bundle/fitbook-linux.zip \
-  $apk/fitbook.apk
+  "$apk"/app-*-release.apk \
+  "$apk"/pipeline/linux/x64/release/bundle/fitbook-linux.zip \
+  "$apk"/fitbook.apk \
+  "$HOME"/windows/fitbook-windows.zip
 git pull
 
 bundle exec fastlane supply --aab build/app/outputs/bundle/release/app-release.aab
