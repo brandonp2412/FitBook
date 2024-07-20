ba4643677a68f36bf0269704997f24aa1e1c8b4b
Brandon Presley <brandon@presley.nz>
2024-07-20 20:09:04 +1200

    Fix msstore.sh

--- scripts/msstore.sh
+++ scripts/msstore.sh
@@ -1,15 +1,39 @@
 #!/bin/sh
 
-client_id=$(yq -r .clientId "$HOME"/.config/msstore.yml)
-client_secret=$(yq -r .clientSecret "$HOME"/.config/msstore.yml)
-tenant_id=$(yq -r .tenantId "$HOME"/.config/msstore.yml)
-access_token=$(curl -X POST https://login.microsoftonline.com/$tenant_id/oauth2/token \
+set -ex
+
+client_id=$(yq -r .clientId "$HOME/.config/msstore.yml")
+client_secret=$(yq -r .clientSecret "$HOME/.config/msstore.yml")
+tenant_id=$(yq -r .tenantId "$HOME/.config/msstore.yml")
+api="https://manage.devcenter.microsoft.com"
+
+access_token=$(curl -X POST "https://login.microsoftonline.com/$tenant_id/oauth2/token" \
   -H "Content-Type: application/x-www-form-urlencoded; charset=utf-8" \
   -d "grant_type=client_credentials" \
   -d "client_id=$client_id" \
   -d "client_secret=$client_secret" \
-  -d "resource=https://api.partner.microsoft.com" | jq -r .access_token)
+  -d "resource=$api" | jq -r .access_token)
+
 app_id=$(yq -r .msix_config.msstore_appId pubspec.yaml)
 
-curl "https://api.partner.microsoft.com/v1.0/ingestion/products" \
-  -H "Authorization: Bearer $access_token"
+submission_response=$(curl -X POST "$api/v1.0/my/applications/$app_id/submissions" \
+  -H "Authorization: Bearer $access_token" \
+  -H "Content-Type: application/json" \
+  -H "Content-Length: 0")
+submission_id=$(echo "$submission_response" | jq -r .id)
+file_upload_url=$(echo "$submission_response" | jq -r .fileUploadUrl)
+
+if [ -z "$submission_id" ]; then
+  echo "Submission failed to create"
+  exit 1
+fi
+
+curl -X PUT "$file_upload_url" \
+  -H "Content-Type: application/octet-stream" \
+  -H "x-ms-blob-type: BlockBlob" \
+  --data-binary "@$1"
+
+curl -X POST "$api/v1.0/my/applications/$app_id/submissions/$submission_id/commit" \
+  -H "Authorization: Bearer $access_token" \
+  -H "Content-Type: application/json" \
+  -H "Content-Length: 0"
