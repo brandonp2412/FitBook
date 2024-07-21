077e764cc1624fdbbfaf49ded4e032da5e7ea799
Brandon Presley <brandon@presley.nz>
2024-07-21 16:10:54 +1200

    Add delete unused foods button

--- lib/settings/delete_records_button.dart
+++ lib/settings/delete_records_button.dart
@@ -2,11 +2,12 @@ import 'package:drift/drift.dart';
 import 'package:fit_book/constants.dart';
 import 'package:fit_book/main.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DeleteRecordsButton extends StatelessWidget {
   final BuildContext pageContext;
 
-  const DeleteRecordsButton({
+  DeleteRecordsButton({
     super.key,
     required this.pageContext,
   });
@@ -110,6 +111,58 @@ class DeleteRecordsButton extends StatelessWidget {
     );
   }
 
+  final formatter = NumberFormat('#,##0');
+
+  deleteUnused(BuildContext sheetContext) {
+    Navigator.pop(sheetContext);
+    showDialog(
+      context: sheetContext,
+      builder: (BuildContext dialogContext) {
+        final unusedFuture = (db.foods.selectOnly()
+              ..addColumns([db.foods.id])
+              ..join(
+                [
+                  leftOuterJoin(
+                    db.entries,
+                    db.entries.food.equalsExp(db.foods.id),
+                  ),
+                ],
+              )
+              ..addColumns([db.entries.id])
+              ..where(db.entries.id.isNull()))
+            .get();
+
+        return AlertDialog(
+          title: const Text('Confirm delete'),
+          content: FutureBuilder(
+            future: unusedFuture,
+            builder: (context, snapshot) => Text(
+              'Are you sure you want to delete ${formatter.format(snapshot.data?.length ?? 0)} unused foods? This action is irreversible',
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(dialogContext);
+              },
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () async {
+                Navigator.pop(dialogContext);
+                final ids = (await unusedFuture)
+                    .map((unused) => unused.read(db.foods.id)!);
+                (db.foods.deleteWhere((tbl) => tbl.id.isIn(ids)));
+                if (pageContext.mounted) Navigator.pop(pageContext);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   deleteDiary(BuildContext sheetContext) {
     Navigator.pop(sheetContext);
     showDialog(
@@ -160,6 +213,11 @@ class DeleteRecordsButton extends StatelessWidget {
                   leading: const Icon(Icons.restaurant),
                   title: const Text('Food'),
                   onTap: () => deleteFoods(sheetContext),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.no_meals),
+                  title: const Text('Unused food'),
+                  onTap: () => deleteUnused(sheetContext),
                 ),
                 ListTile(
                   leading: const Icon(Icons.scale),
