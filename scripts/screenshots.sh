ccf6d27a45b23fd645c9e81672d25fad3379b239
Brandon Presley <brandon@presley.nz>
2024-07-24 15:50:14 +1200

    Run emulator for avd in headless mode

--- scripts/screenshots.sh
+++ scripts/screenshots.sh
@@ -4,30 +4,30 @@ set -e
 
 device_type="$1"
 
-case "$(uname -s)" in 
-  Linux*)
-    echo "Launching $device_type..."
-    $TERMINAL -t Hide emulator -avd "$device_type" -feature -Vulkan \
-      -no-boot-anim -noaudio &> /dev/null &
+case "$(uname -s)" in
+Linux*)
+  echo "Launching $device_type..."
+  $TERMINAL -t Hide emulator -avd "$device_type" -feature -Vulkan \
+    -no-boot-anim -noaudio -no-window &>/dev/null &
 
-    while true; do  
-      for device in $(adb devices | awk 'NR>1{print $1}' | grep emulator); do
-        name=$(
-          adb -s $device emu avd name 2> /dev/null | head -n 1 | tr -d '\r'
-        ) 
-        [ "$name" = "$device_type" ] && break
-      done
-
-      boot_completed=$(
-        adb -s "$device" shell getprop sys.boot_completed 2> /dev/null \
-          | tr -d '\r'
+  while true; do
+    for device in $(adb devices | awk 'NR>1{print $1}' | grep emulator); do
+      name=$(
+        adb -s $device emu avd name 2>/dev/null | head -n 1 | tr -d '\r'
       )
-      adb -s "$device" get-state 2> /dev/null | grep -q device && \
-        [ "$name" = "$device_type" ] && [ "$boot_completed" = "1" ] \
-        && break
-
-      sleep 1
+      [ "$name" = "$device_type" ] && break
     done
+
+    boot_completed=$(
+      adb -s "$device" shell getprop sys.boot_completed 2>/dev/null |
+        tr -d '\r'
+    )
+    adb -s "$device" get-state 2>/dev/null | grep -q device &&
+      [ "$name" = "$device_type" ] && [ "$boot_completed" = "1" ] &&
+      break
+
+    sleep 1
+  done
   ;;
 esac
 
@@ -38,9 +38,9 @@ flutter drive --driver=test_driver/integration_test.dart \
   --dart-define=FLEXIFY_DEVICE_TYPE="$device_type" -d "${device:-$device_type}"
 code=$?
 
-case "$(uname -s)" in 
-  Linux*)
-    adb -s "$device" reboot -p
-    exit $code
+case "$(uname -s)" in
+Linux*)
+  adb -s "$device" reboot -p
+  exit $code
   ;;
 esac
