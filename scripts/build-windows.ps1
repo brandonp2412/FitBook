45a05654f0e19c80034c10217cca2da8a8fcd00c
Brandon Presley <brandon@presley.nz>
2025-02-10 23:25:04 +1300

    Exclude build in robocopy

--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -1,5 +1,5 @@
 Remove-Item -Path fitbook/* -Recurse -Force
-robocopy //host.lan/Data/fitbook-source fitbook /E /Z /R:3 /NFL /NDL /NP
+robocopy //host.lan/Data/fitbook-source fitbook /E /Z /R:3 /NFL /NDL /NP /XD build*
 cd fitbook
 dart run msix:create
-robocopy build/windows/x64/runner/Release //host.lan/Data/fitbook /E /Z /R:3 /NFL /NDL /NP
+robocopy build/windows/x64/runner/Release //host.lan/Data/fitbook /E /Z /R:3 /NFL /NDL /NP /XD build*
