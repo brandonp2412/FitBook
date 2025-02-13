75fa29faada95d0cf80741da5460f7f91d481368
Brandon Presley <brandon@presley.nz>
2025-02-13 13:39:24 +1300

    Update macos.sh

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -7,21 +7,21 @@ export PATH="$FLUTTER_ROOT/bin:$PATH"
 
 set -ex
 
+####
+# iOS deployment 🍏
+####
 flutter build ipa
 /usr/local/bin/fastlane deliver --ipa build/ios/ipa/fit_book.ipa
 
+###
+# MacOS deployment 😬
+###
 flutter build macos --release
+fastlane cert --platform macos --type mac_installer_distribution --user "$APPLE_EMAIL"
 APP_NAME="build/macos/Build/Products/Release/FitBook.app"
 PACKAGE_NAME=build/macos/FitBook.pkg
 xcrun productbuild --component "$APP_NAME" /Applications/ build/macos/unsigned.pkg
-
-INSTALLER_CERT_NAME=$(keychain list-certificates |
-  jq '[.[]
-            | select(.common_name
-            | contains("Mac Developer Installer"))
-            | .common_name][0]' |
-  xargs)
+INSTALLER_CERT_NAME=$(security find-certificate -c "Mac Developer Installer" -a | grep "alis" | head -n 1 | cut -d '"' -f 4)
 xcrun productsign --sign "$INSTALLER_CERT_NAME" build/macos/unsigned.pkg "$PACKAGE_NAME"
 rm -f build/macos/unsigned.pkg
-
-/usr/local/bin/fastlane deliver --pkg build/macos/FitBook.pkg
+fastlane deliver --pkg build/macos/FitBook.pkg --platform osx
