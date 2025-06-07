3e511e1db1c7db7a07807bf39fe2d4e056d55a64
Brandon Presley <brandon@presley.nz>
2025-06-07 16:51:29 +1200

    Handle different systems in screenshots.sh

--- scripts/screenshots.sh
+++ scripts/screenshots.sh
@@ -4,9 +4,33 @@ set -ex
 
 device_type="${1:-phoneScreenshots}"
 
-case "$(uname -s)" in
-Linux*)
-  echo "Launching $device_type..."
+if [[ "$(uname -s)" == Darwin* ]]; then
+  echo "Screenshots not automated yet on MacOS..."
+  exit 1
+elif [[ "$(uname -s)" == Linux* ]] && grep -i -q Microsoft /proc/version 2>/dev/null; then
+  echo "Launching $device_type on WSL..."
+  
+  emulator.exe -avd "$device_type" -no-boot-anim -noaudio -no-window &> /dev/null &
+  while true; do
+    for device in $(adb devices | awk 'NR>1{print $1}' | grep emulator); do
+      name=$(
+        adb -s $device emu avd name 2>/dev/null | head -n 1 | tr -d '\r'
+      )
+      [ "$name" = "$device_type" ] && break
+    done
+
+    boot_completed=$(
+      adb -s "$device" shell getprop sys.boot_completed 2>/dev/null |
+        tr -d '\r'
+    )
+    adb -s "$device" get-state 2>/dev/null | grep -q device &&
+      [ "$name" = "$device_type" ] && [ "$boot_completed" = "1" ] &&
+      break
+
+    sleep 1
+  done
+elif [[ "$(uname -s)" == Linux* ]]; then
+  echo "Launching $device_type on linux..."
   $TERMINAL -t Hide emulator -avd "$device_type" -feature -Vulkan \
     -no-boot-anim -noaudio -no-window &>/dev/null &
 
@@ -28,8 +52,10 @@ Linux*)
 
     sleep 1
   done
-  ;;
-esac
+else
+  echo "Screenshots not supported on your platform."
+  exit 1
+fi
 
 export FLEXIFY_DEVICE_TYPE="$device_type"
 
@@ -38,9 +64,13 @@ export FLEXIFY_DEVICE_TYPE="$device_type"
   --dart-define=FLEXIFY_DEVICE_TYPE="$device_type" -d "${device:-$device_type}"
 code=$?
 
-case "$(uname -s)" in
-Linux*)
+if [[ "$(uname -s)" == Darwin* ]]; then
+  echo "Screenshots not automated yet on MacOS..."
+  exit 1
+elif [[ "$(uname -s)" == Linux* ]]; then
   adb -s "$device" reboot -p
   exit $code
-  ;;
-esac
+else
+  echo "Screenshots not supported on your platform."  
+  exit 1
+fi
