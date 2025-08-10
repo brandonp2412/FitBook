4057799d963e1bd6d8b0a12da78e185e632c26fd
Brandon Presley <brandon@presley.nz>
2025-08-10 14:12:48 +1200

    Use autocomplete in diary filters

--- lib/entry/entry_filters.dart
+++ lib/entry/entry_filters.dart
@@ -1,5 +1,8 @@
+import 'package:drift/drift.dart';
 import 'package:fit_book/entry/entry_state.dart';
+import 'package:fit_book/main.dart';
 import 'package:fit_book/settings/settings_state.dart';
+import 'package:flutter/material.dart' as material;
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -14,7 +17,7 @@ class EntryFilters extends StatefulWidget {
 }
 
 class _EntryFiltersState extends State<EntryFilters> {
-  final groupCtrl = TextEditingController();
+  var groupCtrl = TextEditingController();
 
   @override
   void initState() {
@@ -28,22 +31,50 @@ class _EntryFiltersState extends State<EntryFilters> {
       context: context,
       builder: (dialogContext) => AlertDialog(
         title: const Text('Filters'),
-        content: Column(
+        content: material.Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            TextFormField(
-              controller: groupCtrl,
-              textCapitalization: TextCapitalization.sentences,
-              decoration: const InputDecoration(
-                labelText: 'Food group',
-                hintText: 'Fruit',
-              ),
-              onFieldSubmitted: (value) {
-                context.read<EntryState>().foodGroup =
-                    value.isNotEmpty ? value : null;
-                Navigator.of(dialogContext).pop();
+            Autocomplete<String>(
+              optionsMaxHeight: 300,
+              optionsBuilder: (value) async {
+                if (value.text.isEmpty) return [];
+                final results = await (db.foods.selectOnly()
+                      ..where(
+                        db.foods.foodGroup.like('%${value.text}%') &
+                            db.foods.foodGroup.isNotNull(),
+                      )
+                      ..addColumns([db.foods.foodGroup])
+                      ..groupBy([db.foods.foodGroup]))
+                    .get();
+                return results
+                    .map((result) => result.read(db.foods.foodGroup)!);
               },
-              onEditingComplete: () {},
+              onSelected: (option) async {
+                groupCtrl.text = option;
+                context.read<EntryState>().foodGroup = option;
+              },
+              fieldViewBuilder: (
+                BuildContext context,
+                TextEditingController textEditingController,
+                FocusNode focusNode,
+                VoidCallback onFieldSubmitted,
+              ) {
+                groupCtrl = textEditingController;
+                return TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Food group',
+                    hintText: 'Fruit',
+                  ),
+                  controller: textEditingController,
+                  focusNode: focusNode,
+                  textCapitalization: TextCapitalization.sentences,
+                  onFieldSubmitted: (String value) {
+                    context.read<EntryState>().foodGroup = value;
+                    Navigator.of(context).pop();
+                  },
+                  textInputAction: TextInputAction.next,
+                );
+              },
             ),
             const SizedBox(height: 16),
             ListTile(
